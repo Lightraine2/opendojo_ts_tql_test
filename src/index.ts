0cc9003437364118c89be49bef3671d970563698
Lightraine2 <laurencedale1986@gmail.com>
2021-01-02 23:20:28 +0000

    to 56.20 - graphql up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
 
@@ -18,9 +23,21 @@ const main = async () => {
 //    console.log(posts);
 
     const app = express();
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({app});
+    
     app.listen(4000, () => {
         console.log('server started on localhost:4000')
     });
+
     app.get('/', (req, res) => {
         res.send("Hello world!");
     });

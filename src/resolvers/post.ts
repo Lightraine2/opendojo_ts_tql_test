9c071dfa72ecdb6cb2d887ace7ffedf6d1e4d80a
Lightraine2 <laurencedale1986@gmail.com>
2021-01-03 20:33:13 +0000

    to 1.09.33

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from 'src/types';
-import {Resolver, Query, Ctx, Arg, Int} from 'type-graphql';
+import {Resolver, Query, Ctx, Arg, Int, Mutation} from 'type-graphql';
 import {Post} from '../entities/Post';
 
 // the resolver file determines what can be done with the objects in entities - crud functions. i.e. the post resolver dictates what is done with the post entity
@@ -8,12 +8,49 @@ import {Post} from '../entities/Post';
 export class PostResolver {
 
     @Query(() => Post, {nullable: true})
-    posts(
+    post(
         //specify how to sort the data. i.e. if you search by 'id' it will find one that matches the search param
             // e.g.
         // post(id: 1)
-        @Arg('id', () => Int) id: number,
+        @Arg('id') id: number,
         @Ctx() {em}: MyContext): Promise<Post | null> {
         return em.findOne(Post, {id});
-}
+    }
+
+    @Mutation(() => Post )
+    async createPost(
+
+        @Arg('title') title: string,
+        @Ctx() {em}: MyContext): Promise<Post> {
+        const post = em.create(Post, {title})
+        await em.persistAndFlush(post)
+        return post
+    }
+    
+    @Mutation(() => Post, {nullable: true} )
+    async updatePost(
+        // if something is nullable, you must declare the type
+        @Arg("id") id: number,
+        @Arg("title", () => String, {nullable:true}) title: string,
+        @Ctx() {em}: MyContext): Promise <Post | null> {
+        const post = await em.findOne(Post, {id});
+        if (!post) {
+            return null;
+        }    
+        if (typeof title !== "undefined") {
+        post.title = title;            
+        await em.persistAndFlush(post);
+        }
+
+        return post;
+    }
+
+    @Mutation(() => Boolean )
+    async deletePost(
+        // if something is nullable, you must declare the type
+        @Arg("id") id: number,
+        @Ctx() {em}: MyContext): Promise <boolean> {
+        await em.nativeDelete(Post, { id });    
+        return true;
+    }
 }

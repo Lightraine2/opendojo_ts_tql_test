5e6b4218e5231ca908fe1d569cd396d484b90c5b
Lightraine2 <laurencedale1986@gmail.com>
2021-01-17 22:19:06 +0000

    to 2.38 ready to test

--- opendojo-front/pages/_app.tsx
+++ opendojo-front/pages/_app.tsx
@@ -1,17 +1,22 @@
 import '../styles/globals.css'
 import {ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import {Provider, createClient} from 'urql';
+
+const client = createClient({ url: 'http://localhost:4000/graphql'});
 
 
 import theme from './theme';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider value={client}>
     <ThemeProvider theme={theme}>
       <ColorModeProvider>
       <CSSReset/>
       <Component {...pageProps} />
       </ColorModeProvider>
     </ThemeProvider>
+    </Provider>
     )};
 
 export default MyApp
--- opendojo-front/pages/register.tsx
+++ opendojo-front/pages/register.tsx
@@ -4,18 +4,37 @@ import {Wrapper} from "../components/wrapper";
 import React from 'react';
 import { FormControl, FormErrorMessage, FormLabel, Button, Input, Box }from "@chakra-ui/core";
 import {InputField} from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {
 
 }
 
+const REGISTER_MUTATION = 
+`
+mutation Register($username: String!, $password:String!) {
+    register(options: {username: $username, password: $password}) {
+        errors {
+            field
+            message
+        }
+        user {
+            id
+            username
+        }
+    }
+}
+`
+
+// notice how here we've wrapped the whole app in the urql client - and we'll call the mutations from gql in the components. 
 
 const Register: React.FC<registerProps> = ({}) => {
+    const [,register] = useMutation(REGISTER_MUTATION);
     return (
         <Wrapper variant="small">
         <Formik initialValues={{ username: "", password: ""}}
         onSubmit={(values) => {
-            console.log(values);
+            register(values);
         }}>
             {({isSubmitting}) => (
                 <Form>

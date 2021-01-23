206ed792cea494fb1cd5e471488a3e4b976824ea
Lightraine2 <laurencedale1986@gmail.com>
2021-01-23 22:04:05 +0000

    to 2.57.59

--- opendojo-front/pages/register.tsx
+++ opendojo-front/pages/register.tsx
@@ -1,41 +1,35 @@
 import { resolveHref } from "next/dist/next-server/lib/router/router";
 import {Formik, Form} from 'formik';
-import {Wrapper} from "../components/wrapper";
+import {Wrapper} from "../components/Wrapper";
 import React from 'react';
 import { FormControl, FormErrorMessage, FormLabel, Button, Input, Box }from "@chakra-ui/core";
 import {InputField} from "../components/InputField";
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {
 
 }
 
-const REGISTER_MUTATION = 
-`
-mutation Register($username: String!, $password:String!) {
-    register(options: {username: $username, password: $password}) {
-        errors {
-            field
-            message
-        }
-        user {
-            id
-            username
-        }
-    }
-}
-`
+
 
 // notice how here we've wrapped the whole app in the urql client - and we'll call the mutations from gql in the components. 
 
 const Register: React.FC<registerProps> = ({}) => {
-    const [,register] = useMutation(REGISTER_MUTATION);
+    const [,register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
         <Formik 
         initialValues={{ username: "", password: ""}}
-        onSubmit={async (values) => {
+        onSubmit={async (values, {setErrors}) => {
          const response = await register(values);
+         if (response.data?.register.errors) {
+             [{field: 'username', message: 'something wrong'}]
+            setErrors(
+                {
+                    username: "error"
+                }
+            )
+         }
         }}
         >
             {({isSubmitting}) => (
